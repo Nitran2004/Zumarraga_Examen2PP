ca6926e95dce47a951d2206ca4347408bb6a04fa
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-12-06 07:50:30 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Zumarraga_Examen2P;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Zumarraga_Examen2P
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalculateClicked(object sender, EventArgs e)
+        {
+            string input = InputEntry.Text;
+
+            int totalLetters = input.Count(char.IsLetter);
+            int totalNumbers = input.Count(char.IsDigit);
+            int totalVowels = input.Count(c => "aeiouAEIOU".Contains(c));
+            int totalUppercase = input.Count(char.IsUpper);
+            int totalLowercase = input.Count(char.IsLower);
+            int totalCharacters = input.Length;
+
+            LetterCountLabel.Text = $"Total de letras: {totalLetters}\n";
+            ResultLabel.Text = $"Total de números: {totalNumbers}\n" +
+                               $"Total de mayúsculas: {totalUppercase}\n" +
+                               $"Total de minúsculas: {totalLowercase}\n" +
+                               $"Total de vocales: {totalVowels}\n";
+                
+            CharacterCountLabel.Text = $"Total de caracteres: {totalCharacters}\n";
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            InputEntry.Text = string.Empty;
+            CharacterCountLabel.Text = string.Empty;
+            LetterCountLabel.Text = string.Empty;
+            ResultLabel.Text = string.Empty;
+        }
+    }
+}
